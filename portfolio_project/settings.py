669770afcc4df0211bf25de54387fbfe029eef52
M.Adel <ndm.eis.mail@gmail.com>
2026-04-01 11:03:57 +0200

    fix: use correct WhiteNoise storage class name

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -142,7 +142,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.WhiteNoiseStorage",
+        "BACKEND": "whitenoise.storage.StaticFilesStorage",
     },
 }
 
